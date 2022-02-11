62714433cbb4114ff01b4dc2a97ec8b6d819fb70
syedahmedusman2 <66121118+syedahmedusman2@users.noreply.github.com>
2022-02-12 02:00:02 +0500

    Update constants.dart

--- bookapp/lib/Constants/constants.dart
+++ bookapp/lib/Constants/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get_storage/get_storage.dart';
 
 const Color primaryColor = Color(0xffF7DD74);
 String fontfamily = "Montserrat-Regular";
-
+final box = GetStorage();
 const Color fontColor = Color(0xffb8c2cc);
